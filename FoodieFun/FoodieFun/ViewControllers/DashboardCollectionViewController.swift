25190dfc643cf6aa15e98cfdee07a1c4a8f8d438
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-06 16:11:36 -0500

    refactoring

--- FoodieFun/FoodieFun/ViewControllers/DashboardCollectionViewController.swift
+++ FoodieFun/FoodieFun/ViewControllers/DashboardCollectionViewController.swift
@@ -21,24 +21,13 @@ class DashboardCollectionViewController: UICollectionViewController {
     // Hold the restaurants that the user is searching for
     var filteredRestaurant = [Restaurant]()
     
-//    @IBOutlet weak var searchFooter: SearchFooter!
-//    @IBOutlet weak var searchFooterBottomConstraint: NSLayoutConstraint!
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // communication between Add tab and Home tab
         NotificationCenter.default.addObserver(self, selector: #selector(onDidReceiveData), name: .restaurantDidSaveNotification, object: nil)
         
-        // Set up the search controller
-        // Allows the class to be informed as text changes within the UISearchBar
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Restaurant"
-        navigationItem.searchController = searchController
-        
-        // Ensure the serach bar does not remain on the screen if the user navigates to another view controller
-        definesPresentationContext = true
+        self.search()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -71,6 +60,18 @@ class DashboardCollectionViewController: UICollectionViewController {
     }
     
     // for Search Functionality
+    func search() {
+        // Set up the search controller
+        // Allows the class to be informed as text changes within the UISearchBar
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Restaurant by name, location or type of cuisine"
+        navigationItem.searchController = searchController
+        
+        // Ensure the serach bar does not remain on the screen if the user navigates to another view controller
+        definesPresentationContext = true
+    }
+    
     func aRestaurant(_ keyword: String) -> [Restaurant] {
         let restaurants = self.restaurantController.restaurants
         
