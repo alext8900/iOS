4806b496699b3fcb8bc747e81a5a094bb37b9b25
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-11 21:24:16 -0500

    updating the ui tests

--- FoodieFun/FoodieFunUITests/FoodieFunUITests.swift
+++ FoodieFun/FoodieFunUITests/FoodieFunUITests.swift
@@ -10,18 +10,6 @@ import XCTest
 @testable import FoodieFun
 
 class FoodieFunUITests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testLoggingIn() {
         
         let app = XCUIApplication()
